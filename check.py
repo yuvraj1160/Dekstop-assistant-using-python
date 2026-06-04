fe25f9617287dc246562e7888f1995f3277a82c1
yuvraj1160 <yuvrathod1610@gmail.com>
2026-06-04 21:57:46 +0530

    remove check.py

--- check.py
+++
@@ -1,3 +0,0 @@
-import speech_recognition as sr
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print(f"Microphone with name \"{name}\" found for `Microphone(device_index={index})`")
